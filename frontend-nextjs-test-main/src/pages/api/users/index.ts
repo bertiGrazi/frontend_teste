b5658de32d2c9a7efbf79be0d3ab284958c6cb40
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-01-25 20:21:37 -0300

    set users API and rendering it on screen

--- frontend-nextjs-test-main/src/pages/api/users/index.ts
+++ frontend-nextjs-test-main/src/pages/api/users/index.ts
@@ -15,7 +15,28 @@ import { NextApiRequest, NextApiResponse } from 'next/types';
 import { IUser } from '@/types/user.d';
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-	const users: Array<unknown> = [];
 
-	return res.status(500).json(users);
+	const users: IUser[] = [
+    {
+      id: 323,
+      name: 'Usuário 323',
+      email: 'user-323@mail.com',
+    },
+    {
+      id: 324,
+      name: 'Usuário 324',
+      email: 'user-324@mail.com',
+    },
+		{
+      id: 325,
+      name: 'Usuário 325',
+      email: 'user-325@mail.com',
+    },
+  ];
+
+	if (req.method === 'GET') {
+    return res.status(200).json(users);
+  } else {
+    return res.status(500).json(users);
+  }
 };
